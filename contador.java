b2e77ddd8518f27572f64c4b03d1add1d305eb81
Wildes Sousa <74382948+Sousa-Diin@users.noreply.github.com>
2024-06-08 09:56:50 -0300

    :hammer: recuperar aplicação na exception entrada invalida

--- contador.java
+++ contador.java
@@ -4,12 +4,33 @@ import java.util.Scanner;
 public class Contador {
 	/**
 	 * @param args
+	 * @throws ParametrosIvalidosException 
 	 */
-	public static void main(String[] args) {
-		Scanner terminal = new Scanner(System.in);
+	public static void main(String[] args) throws ParametrosIvalidosException {
 	
-		try {
+		processar_entrada();
+		System.out.println("FIM da execução! - [Main]");
+	}
+
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosIvalidosException  {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if ( parametroUm >= parametroDois ){
+            //System.err.println("O primeiro parametro precisar ser menor que o segundo");
+			throw new ParametrosIvalidosException ();
+		}
+		
+		int contagem = parametroDois - parametroUm;
+		//realizar o for para imprimir os números com base na variável contagem
+		for (int i = 1; i <= contagem;i++){
 			
+			System.out.println("Imprima o numero " + i);
+		}
+	}
+
+	static void processar_entrada () throws ParametrosIvalidosException{
+		
+		Scanner terminal = new Scanner(System.in);
+		try {
 			System.out.println("Digite o primeiro parâmetro");
 			int parametroUm = terminal.nextInt();
 			System.out.println("Digite o segundo parâmetro");
@@ -25,22 +46,11 @@ public class Contador {
 			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
 			System.err.println(e);
 		}
-		catch ( InputMismatchException e ){
+		catch ( InputMismatchException e ){ // tentativa de recuperar a aplicação sem finally
 			System.out.println("Por favor digite um número válido.");
-		}
-	}
-	static void contar(int parametroUm, int parametroDois ) throws ParametrosIvalidosException  {
-		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-        if ( parametroUm >= parametroDois ){
-            //System.err.println("O primeiro parametro precisar ser menor que o segundo");
-			throw new ParametrosIvalidosException ();
+			processar_entrada(); // chama metodo processar entrada novamente caso um dos valores seja invalidos
+			System.out.println("FIM da execução! - [InputMismatchException]");
 		}
 		
-		int contagem = parametroDois - parametroUm;
-		//realizar o for para imprimir os números com base na variável contagem
-		for (int i = 1; i <= contagem;i++){
-			
-			System.out.println("Imprima o numero " + i);
-		}
 	}
 }
